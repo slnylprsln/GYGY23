3f73652b14f9faaac214a8e539e4ea4c9654716f
slnylprsln <alparslanselenay@gmail.com>
2023-03-26 15:40:09 +0300

    Covariance and Contravariance are added

---
+++ CovarianceAndContravariance/Contravariance.cs
@@ -0,0 +1,33 @@
+﻿// Contravariance örneği
+/*
+ * Covariance ve Contravariance, birbirinden türetilmiş tiplerin kendi arasında tür dönüşümü yapılması, 
+ * birbiriyle yer değiştirilebilmesi ve eşitlikleri gibi karşılaştığımız belirli durumlardır.
+ * 
+ * Contravariance: Küçük türlerin (less derived) daha büyük türlere (more derived) dönüştürülmesi durumu.
+ */
+
+class Vehicle { }
+class Car : Vehicle { }
+class Bus : Vehicle { }
+
+class Contravariance
+{
+    static void Main(string[] args)
+    {
+        Action<Vehicle> vehicleAction = StartVehicle;
+        Action<Car> carAction = StartCar;
+
+        // Contravariance sayesinde, Action<Vehicle> türünden bir değişkene
+        // Action<Car> türünden bir değer atanabilir.
+        carAction = vehicleAction;
+
+        Vehicle vehicle = new Vehicle();
+        carAction(vehicle); // Hata verir, Vehicle türü için yazılmış metot çağrılamaz
+
+        Car car = new Car();
+        carAction(car); // Çalışır, Car türü için yazılmış metot çağrılabilir
+    }
+
+    static void StartVehicle(Vehicle vehicle) => Console.WriteLine("Starting vehicle...");
+    static void StartCar(Car car) => Console.WriteLine("Starting car...");
+}
---
+++ CovarianceAndContravariance/Covariance.cs
@@ -0,0 +1,30 @@
+﻿// Covariance örneği
+/*
+ * Covariance ve Contravariance, birbirinden türetilmiş tiplerin kendi arasında tür dönüşümü yapılması, 
+ * birbiriyle yer değiştirilebilmesi ve eşitlikleri gibi karşılaştığımız belirli durumlardır.
+ * 
+ * Covariance: Geniş türlerin (more derived) daha küçük türlere (less derived) dönüştürülmesi durumu.
+ */
+
+class Animal { }
+class Cat : Animal { }
+
+class Covariance
+{
+    static void Main(string[] args)
+    {
+        Func<Animal> animalFactory = CreateAnimal;
+        Func<Cat> catFactory = CreateCat;
+
+        // Covariance sayesinde, Func<Animal> türünden bir değişkene
+        // Func<Cat> türünden bir değer atanabilir.
+        animalFactory = catFactory;
+
+        Animal animal = animalFactory();
+
+        Console.WriteLine(animal.GetType());
+    }
+
+    static Animal CreateAnimal() => new Animal();
+    static Cat CreateCat() => new Cat();
+}
